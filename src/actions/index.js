d9987c9b1ad12aa32c859c14e830841271d61cd5
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-03-28 09:54:07 -0600

    finished

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export function selectBook(book) {
+	//selectBook is an ActionCreator, it needs to return an action
+	// an object with a type property, an sometimes a payload
+	
+	return {
+		type: 'BOOK_SELECTED',
+		payload: book
+	};
+}
+
